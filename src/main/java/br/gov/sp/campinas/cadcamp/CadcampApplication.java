1aeddbfb07469c7b6de5cb1f0be52b1d33423e71
Daniel K I Ikeuchi <daniel.ikeuchi@gmail.com>
2022-10-16 07:41:27 -0300

    Projeto criado

---
+++ src/main/java/br/gov/sp/campinas/cadcamp/CadcampApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.campinas.cadcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/campinas/cadcamp/CadcampApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.campinas.cadcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
